f90d5f12e35259baeef1d023f3aa46f810a775ed
sashaReact <kuyancev1985@mail.ru>
2023-11-08 14:06:50 +0300

    учебный проект от МФТИ первый коммит

---
+++ NewsStudyRtk/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    context = {'title': 'test page',
+               'Header1': 'Заголовок страницы'}
+    return render(request, 'main/index.html', context)
+def about(request):
+    return HttpResponse('<h1 style="color: blue" > О нас </h1>')
+def contacts(request):
+    return HttpResponse('<h1 style="color: magenta" > Контакты </h1>')
+
+def home(request):
+    return HttpResponse('<h1 style="color: magenta" > Тестовая страница </h1>')
