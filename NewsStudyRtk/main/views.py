33efc74a6cb55cbd16b6d0eb38b0758570d2d9cf
sashaReact <kuyancev1985@mail.ru>
2023-11-08 16:30:24 +0300

    первый коммит

--- NewsStudyRtk/main/views.py
+++ NewsStudyRtk/main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     context = {'title': 'test page',
-               'Header1': 'Заголовок страницы'}
+               'Header1': 'Заголовок'}
     return render(request, 'main/index.html', context)
 def about(request):
     return HttpResponse('<h1 style="color: blue" > О нас </h1>')
