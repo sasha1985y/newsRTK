0efbca7312f3f10e091acbaa756f637a802749b9
sashaReact <kuyancev1985@mail.ru>
2023-11-09 12:56:18 +0300

    удалил почти все тренировочные файлы из этой ветки

--- NewsStudyRtk/NewsStudyRtk/settings.py
+++ NewsStudyRtk/NewsStudyRtk/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
     'main',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
--- NewsStudyRtk/main/urls.py
+++ NewsStudyRtk/main/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 
 from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('contacts/', views.contacts, name='contact'),
+    path('sidebar/', views.sidebar),
 ]
