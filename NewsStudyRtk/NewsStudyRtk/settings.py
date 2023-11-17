cb2f4287c21307cf7172f13a1c7e1928227da8b7
sasha1985y <kuyancev1985@mail.ru>
2023-11-17 18:16:51 +0300

    добавил несколько новых страниц

--- NewsStudyRtk/NewsStudyRtk/settings.py
+++ NewsStudyRtk/NewsStudyRtk/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'NewsStudyRtk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'all_apps/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
