85e11da84f7e327a4c20e465e31d2f1e2b8ceb11
sasha1985y <kuyancev1985@mail.ru>
2023-12-13 05:12:12 +0300

    пильнул блок юзера

--- NewsStudyRtk/users/views.py
+++ NewsStudyRtk/users/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 from .forms import *
+
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import Group
+
+def registration(request):
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save() #появляется новый пользователь
+            group = Group.objects.get(name='Authors')
+            user.groups.add(group)
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            #!!!не аутентифицируется - нужно доделать
+            authenticate(username=username,password=password)
+            messages.success(request,f'{username} был зарегистрирован!')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    context = {'form':form}
+    return render(request,'users/registration.html',context)
 
 def contact_page(request):
     if request.method == "POST":
