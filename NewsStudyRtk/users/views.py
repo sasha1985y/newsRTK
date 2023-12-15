81f50768996a9eaeb9666233a9d3f45d3f94bf6b
sasha1985y <kuyancev1985@mail.ru>
2023-12-15 18:42:07 +0300

    продолжаю пилить меню юзера

--- NewsStudyRtk/users/views.py
+++ NewsStudyRtk/users/views.py
@@ -41,14 +41,6 @@ def contact_page(request):
     return render(request,'users/contact_page.html',context)
 
 def index(request):
-    print(request.user, request.user.id)
     user_acc = Account.objects.get(user=request.user)
-    print(user_acc.birthdate, user_acc.gender)
     context = {'user_acc': user_acc}
     return render(request,'users/account.html', context)
-
-# def index(request):
-#     print(request.user,request.user.id)
-#     user_acc = Account.objects.get(user=request.user)
-#     print(user_acc.birthdate, user_acc.gender)
-#     return HttpResponse('Приложение Users')
