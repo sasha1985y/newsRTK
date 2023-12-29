01a6d46454c286be8cad5d4df887d979935c1ed7
sasha1985y <kuyancev1985@mail.ru>
2023-12-29 19:55:14 +0300

    переехал на postges

--- NewsStudyRtk/NewsStudyRtk/settings.py
+++ NewsStudyRtk/NewsStudyRtk/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'NewsStudyRtk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deploy53db',#env("DB_NAME"),
+        'USER': 'postgres',#env("DB_USER"),
+        'PASSWORD': 'sashapostgres',#env("DB_PASSWORD"),
+        'HOST': '127.0.0.1',#env("DB_HOST"),
+        'PORT': '5432',#env("DB_PORT"),
     }
 }
 
