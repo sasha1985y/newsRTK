95471ac5c7456b3ce33602cd4a6a4476567938aa
sasha1985y <kuyancev1985@mail.ru>
2024-01-14 22:35:37 +0300

    переопределил местоположение папки static, буду колдовать над ckeditor

--- NewsStudyRtk/NewsStudyRtk/settings.py
+++ NewsStudyRtk/NewsStudyRtk/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'crispy_forms',
     'crispy_bootstrap5',
+    'ckeditor',
+    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
@@ -146,6 +148,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "home/static/",
+    ]
+#STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
